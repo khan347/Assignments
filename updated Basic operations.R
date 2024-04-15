55883a28ab66127baed381988bf63b1d2c5d842a
khan347 <mohdmehboobkhan72@gmail.com>
2024-04-15 00:47:33 -0400

    added file

---
+++ updated Basic operations.R
@@ -0,0 +1,34 @@
+H <-56. #add
+T <-65
+Z <-H+T
+Z
+
+F <- H*T #multiply
+F
+  
+J <-6 #divide
+I <-2
+M <-J/I
+M
+
+# Logical Operators Example
+age <- 16
+
+x <- 5
+if (x > 0) {
+  print("x is positive")
+} else {
+  print("x is non-positive")
+}
+ X  <- 5
+ Y <- 6
+ X & Y
+
+number <- 50 < 45
+number
+
+number2 <- (6<7)
+number2
+
+number & number2
+number | number2

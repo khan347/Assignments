704bf250549d8edff4ac93885729dbcd2cabe0dd
khan347 <166888733+khan347@users.noreply.github.com>
2024-04-19 23:50:04 -0400

    4-4-24 classwork 1

---
+++ Classwork 1.R
@@ -0,0 +1,75 @@
+X <- "hello world"
+X
+#assisting variables
+A <-5
+B <-10
+C <- A + B
+C
+#variables
+v1 <- 4.5
+v2 <- 2
+v3 <- v1 / v2
+v3
+v4 <- sqrt(v1)
+v4
+#more examples of variables
+greet <-"Hello,"
+name <-"bob"
+message <- paste(greet, name)
+message
+#integers
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 2+3i
+typeof(z)
+
+#haracter
+a <- "h"
+typeof(a)
+
+#logical
+q <- T
+typeof(q)
+p <- FALSE
+typeof(p)
+
+
+a <- 5
+b <- 3
+c <- a + b
+d <- a - b
+e <- a * b
+f <- a / b
+
+a
+b
+c
+d
+e
+f
+
+15<5
+20<25
+70==30
+result<-5<5
+result
+typeof(result)
+
+#basic logics
+result <- 4 < 5
+result
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+
+
+
+
